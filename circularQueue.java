58fbdec4db860a06442105b3e91b4572db630ea9
Namanmittha7 <seenumittha7@gmail.com>
2025-09-22 23:41:09 +0530

    Create circularQueue.java

---
+++ circularQueue.java
@@ -0,0 +1,67 @@
+package QUEUE;
+
+public class circularQueue {
+    private int front = -1;
+    private int rare = -1;
+    private int size = 0;
+    private int arr[] = new int[10];
+    private int n = arr.length;
+
+    public void Add(int val) throws Exception { // TC - O(1)
+        if (size == arr.length) {
+            throw new Exception("Queue is full...");
+        }
+        if (size == 0) {
+            front = rare = 0;
+            arr[0] = val;
+        } else if (rare < n - 1) {
+            arr[++rare] = val;
+        } else if (rare == n - 1 && front != 0) {
+            rare = 0;
+            arr[0] = val;
+        }
+        size++;
+    }
+
+    public int remove() throws Exception {  // TC - O(1)
+        if (size == 0) {
+            throw new Exception("queue is empty");
+        }
+        int val = arr[front];
+        if (front == n - 1) {
+            front = 0;
+        } else {
+            front++;
+        }
+        size--;
+        return val;
+
+    }
+
+    public int peek() throws Exception { // TC - O(1)
+        if (size == 0) {
+            throw new Exception("queue is empty");
+        }
+        return arr[front];
+    }
+
+    public void display() throws Exception { // TC - O(N + N) in case of rare < front so overall TC - O(N)
+        if (size == 0) {
+            throw new Exception("queue is empty");
+        }
+        if (front <= rare) {
+            for (int i = front; i <= rare; i++) {
+                System.out.print(arr[i] + " ");
+            }
+        } else {
+            // rare < front;
+            for (int i = front; i < arr.length; i++) {
+                System.out.print(arr[i] + " ");
+            }
+            for (int i = 0; i <= rare; i++) {
+                System.out.print(arr[i] + " ");
+
+            }
+        }
+    }
+}
